5b60546f2e17391c3aa16d033b7bb991deb21036
gituser2343 <rev6525@gmail.com>
2026-02-01 16:04:20 -0500

    Add files via upload

---
+++ LoginApp/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.loginapp
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var v1: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        v1 = findViewById(R.id.button)
+        v1.setOnClickListener {
+            Toast.makeText(this, "You just clicked the Login Button!",
+                Toast.LENGTH_LONG).show()
+        }
+    }
+}
